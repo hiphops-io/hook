ef05e9256f1c4d4479594ea2596af2199604b224
Lukas Oberhuber <lukas@hiphops.io>
2025-07-04 00:36:38 +0100

    chore: release version 0.0.1-alpha28

--- clients/python/scripts/install.py
+++ clients/python/scripts/install.py
@@ -23,7 +23,7 @@ from hiphops_hook.exceptions import DownloadError
 def get_version() -> str:
     """Get the package version."""
     # This version is automatically updated by the release script
-    return "0.0.1-alpha27"
+    return "0.0.1-alpha28"
 
 
 def download_binary() -> None:
