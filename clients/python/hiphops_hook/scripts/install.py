79c40af4780e315624f17d86f99d7852930adb3f
Lukas Oberhuber <lukas@hiphops.io>
2025-07-04 01:40:01 +0100

    chore: release version 0.0.1-alpha30

--- clients/python/hiphops_hook/scripts/install.py
+++ clients/python/hiphops_hook/scripts/install.py
@@ -23,7 +23,7 @@ from hiphops_hook.exceptions import DownloadError
 def get_version() -> str:
     """Get the package version."""
     # This version is automatically updated by the release script
-    return "0.0.1-alpha29"
+    return "0.0.1-alpha30"
 
 
 def download_binary() -> None:
